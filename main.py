6ab21ea0bca8d97ecd9cbd659eee4996e00b75a3
建业周 <jianyezhou@jianyezhoudeMacBook-Pro.local>
2019-01-20 21:22:00 +0800

    修改文件

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from order_proc import OrderPredicts
 from algorithm import Algorithm
+from bayes import Bayes
 
 '''
     进行数据处理
@@ -10,5 +11,5 @@ path = order_proc.data_proc()
     采用算法进行处理
 '''
 if path!="":
-    handle_algorithm = Algorithm(path)
+    handle_algorithm = Bayes(path)
     handle_algorithm.bayes_formutiple()
